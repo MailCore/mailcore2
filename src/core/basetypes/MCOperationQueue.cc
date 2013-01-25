00addb44ea78b4b6c7212b8b460b73857a4f99b9
DINH Viet Hoa <hoa@sprw.me>
2013-01-24 23:35:02 -0800

    cleanup

--- src/core/basetypes/MCOperationQueue.cc
+++ src/core/basetypes/MCOperationQueue.cc
@@ -15,12 +15,8 @@ OperationQueue::OperationQueue()
 {
 	mOperations = new Array();
 	mStarted = false;
-    //sem_init(&mOperationSem, 0, 0);
-    //sem_init(&mStartSem, 0, 0);
-    //sem_init(&mStopSem, 0, 0);
     pthread_mutex_init(&mLock, NULL);
     mWaiting = false;
-    //sem_init(&mWaitingFinishedSem, 0, 0);
     mOperationSem = mailsem_new();
     mStartSem = mailsem_new();
     mStopSem = mailsem_new();
@@ -30,11 +26,7 @@ OperationQueue::OperationQueue()
 OperationQueue::~OperationQueue()
 {
     MC_SAFE_RELEASE(mOperations);
-    //sem_destroy(&mOperationSem);
-    //sem_destroy(&mStartSem);
-    //sem_destroy(&mStopSem);
     pthread_mutex_destroy(&mLock);
-    //sem_destroy(&mWaitingFinishedSem);
     mailsem_free(mOperationSem);
     mailsem_free(mStartSem);
     mailsem_free(mStopSem);
@@ -46,7 +38,6 @@ void OperationQueue::addOperation(Operation * op)
     pthread_mutex_lock(&mLock);
     mOperations->addObject(op);
     pthread_mutex_unlock(&mLock);
-    //sem_post(&mOperationSem);
     mailsem_up(mOperationSem);
     startThread();
 }
@@ -59,7 +50,6 @@ void OperationQueue::runOperationsOnThread(OperationQueue * queue)
 void OperationQueue::runOperations()
 {
     MCLog("start thread");
-    //sem_post(&mStartSem);
     mailsem_up(mStartSem);
     
     while (true) {
@@ -68,15 +58,7 @@ void OperationQueue::runOperations()
 
         AutoreleasePool * pool = new AutoreleasePool();
         
-        //int value = 0;
-        //int r;
-        
-        //r = sem_getvalue(&mOperationSem, &value);
-        //MCLog("x before sem %i %i", value, r);
-        //sem_wait(&mOperationSem);
         mailsem_down(mOperationSem);
-        //sem_getvalue(&mOperationSem, &value);
-        //MCLog("x after sem %i", value);
         
         pthread_mutex_lock(&mLock);
         if (mOperations->count() > 0) {
@@ -85,7 +67,7 @@ void OperationQueue::runOperations()
         pthread_mutex_unlock(&mLock);
 
         if (op == NULL) {
-            //sem_post(&mStopSem);
+            MCLog("stopping %p", this);
             mailsem_up(mStopSem);
             pool->release();
             break;
@@ -111,12 +93,13 @@ void OperationQueue::runOperations()
         
         if (needsCheckRunning) {
             retain(); // (1)
+            MCLog("check running %p", this);
             performMethodOnMainThread((Object::Method) &OperationQueue::checkRunningOnMainThread, this);
         }
         
         pool->release();
     }
-    MCLog("cleanup thread");
+    MCLog("cleanup thread %p", this);
 }
 
 void OperationQueue::callbackOnMainThread(Operation * op)
@@ -135,9 +118,13 @@ void OperationQueue::checkRunningAfterDelay(void * context)
 {
     bool quitting = false;
     
+    //MCLog("started %i", mStarted);
+    if (!mStarted)
+        return;
+    
     pthread_mutex_lock(&mLock);
     if (mOperations->count() == 0) {
-        //sem_post(&mOperationSem);
+        MCLog("trying to quit %p", this);
         mailsem_up(mOperationSem);
         quitting = true;
     }
@@ -147,7 +134,6 @@ void OperationQueue::checkRunningAfterDelay(void * context)
     // And addOperation() should also be called from main thread.
     
     if (quitting) {
-        //sem_wait(&mStopSem);
         mailsem_down(mStopSem);
         mStarted = false;
     }
@@ -162,7 +148,6 @@ void OperationQueue::startThread()
     
     mStarted = true;
     pthread_create(&mThreadID, NULL, (void * (*)(void *)) OperationQueue::runOperationsOnThread, this);
-    //sem_wait(&mStartSem);
     mailsem_down(mStartSem);
 }
 
