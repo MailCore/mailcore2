fd0bb4d4c1e82c8471d7b762df436fe67e3ef1a8
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-04-03 00:45:07 -0700

    Added mailcore::AutoreleasePool on each event handler

---
+++ example/mac/macExample/macExample/MCTApplication.h
@@ -0,0 +1,13 @@
+//
+//  MCTApplication.h
+//  macExample
+//
+//  Created by DINH Viêt Hoà on 4/3/13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MCTApplication : NSApplication
+
+@end
