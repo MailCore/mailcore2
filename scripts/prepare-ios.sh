f82d9d2f598968992c44062861544f6c7980952d
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-04-11 20:41:14 -0700

    improved prepare scripts

--- scripts/prepare-ios.sh
+++ scripts/prepare-ios.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-./prepare-tidy-ios.sh
-./prepare-icu4c-ios.sh
-./prepare-libetpan-ios.sh
-./prepare-ctemplate-ios.sh
+if test ! -d ../Externals/tidy-html5-ios ; then
+  ./prepare-tidy-ios.sh
+fi
+if test ! -d ../Externals/icu4c-ios ; then
+  ./prepare-icu4c-ios.sh
+fi
+if test ! -d ../Externals/libetpan-ios ; then
+  ./prepare-libetpan-ios.sh
+fi
+if test ! -d ../Externals/ctemplate-ios ; then
+  ./prepare-ctemplate-ios.sh
+fi
--- scripts/prepare-mac.sh
+++ scripts/prepare-mac.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-./prepare-icu4c-macos.sh
-./prepare-libetpan-macos.sh
-./prepare-ctemplate-macos.sh
+if test ! -d ../Externals/icu4c ; then
+  ./prepare-icu4c-macos.sh
+fi
+if test ! -d ../Externals/libetpan ; then
+  ./prepare-libetpan-macos.sh
+fi
+if test ! -d ../Externals/ctemplate ; then
+  ./prepare-ctemplate-macos.sh
+fi
+
