71ea31131df617644c7cd18e18324c553a34a989
CodaFi <devteam.codafi@gmail.com>
2013-04-16 17:30:39 -0600

    Made headers public

---
+++ src/objc/provider/MCOProviders.h
@@ -0,0 +1,16 @@
+//
+//  MCOProviders.h
+//  mailcore2
+//
+//  Created by Robert Widmann on 4/16/13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#ifndef __MAILCORE_MCOPROVIDERS_H_
+#define __MAILCORE_MCOPROVIDERS_H_
+
+#import <MailCore/MCOMailProvidersManager.h>
+#import <MailCore/MCOMailProvider.h>
+#import <MailCore/MCONetService.h>
+
+#endif
