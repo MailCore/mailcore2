ed8653070f9daa65d6ffbb8fd30435eed65b3844
Matt Ronge <mronge@mronge.com>
2013-06-08 00:22:33 -0500

    Documented the Obj-C API for providers

--- src/objc/provider/MCOMailProvider.h
+++ src/objc/provider/MCOMailProvider.h
@@ -8,25 +8,79 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+   Represents a email service provider, like for example Gmail, Yahoo, Fastmail.fm etc.
+*/
+
 @interface MCOMailProvider : NSObject
 
 @property (nonatomic, copy) NSString * identifier;
 
 - (id) initWithInfo:(NSDictionary *)info;
 
+/**
+   A list of ways that you can connect to the IMAP server
+   @return An array of MCONetService
+*/
 - (NSArray *) imapServices;
+
+/**
+   A list of ways that you can connect to the SMTP server
+   @return An array of MCONetService
+*/
 - (NSArray *) smtpServices;
+
+/**
+   A list of ways that you can connect to the POP3 server
+   @return An array of MCONetService
+*/
 - (NSArray *) popServices;
 
 - (BOOL) matchEmail:(NSString *)email;
 - (BOOL) matchMX:(NSString *)hostname;
 
+/**
+   Where sent mail is stored on the IMAP server
+   @return Returns nil if it is unknown
+*/
 - (NSString *) sentMailFolderPath;
+
+/**
+   Where starred mail is stored on the IMAP server.
+   This only applies to some servers like Gmail
+   @return Returns nil if it is unknown
+*/
 - (NSString *) starredFolderPath;
+
+/**
+   Where all mail or the archive folder is stored on the IMAP server
+   @return Returns nil if it is unknown
+*/
 - (NSString *) allMailFolderPath;
+
+/**
+   Where trash is stored on the IMAP server
+   @return Returns nil if it is unknown
+*/
 - (NSString *) trashFolderPath;
+
+/**
+   Where draft messages are stored on the IMAP server
+   @return Returns nil if it is unknown
+*/
 - (NSString *) draftsFolderPath;
+
+/**
+   Where spam messages are stored on the IMAP server
+   @return Returns nil if it is unknown
+*/
 - (NSString *) spamFolderPath;
+
+/**
+   Where important messages are stored on the IMAP server
+   This only applies to some servers, like Gmail
+   @return Returns nil if it is unknown
+*/
 - (NSString *) importantFolderPath;
 
 @end
