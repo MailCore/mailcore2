5ccd26d4b13b01395c025e9c0ece11ba069fc8ab
CodaFi <devteam.codafi@gmail.com>
2013-06-21 12:52:20 -0600

    Guards

--- src/core/basetypes/MCAssert.h
+++ src/core/basetypes/MCAssert.h
@@ -2,10 +2,14 @@
 
 #define __MAILCORE_MCASSERT_H_
 
-#include "MCUtils.h"
-
 #define MCAssert(cond) MCAssertInternal(__FILE__, __LINE__, cond, #cond)
 
-MAILCORE_EXTERN void MCAssertInternal(const char * filename, unsigned int line, int cond, const char * condString);
+#ifdef __cplusplus
+extern "C" {
+#endif	
+	void MCAssertInternal(const char * filename, unsigned int line, int cond, const char * condString);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
