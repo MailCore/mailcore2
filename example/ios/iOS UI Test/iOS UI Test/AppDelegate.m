cf3d7e5b4438eb47973ca7dc1a657cd18e5a12dd
Paul Young <paulyoungonline@gmail.com>
2013-06-22 16:15:11 -0400

    Added switch for fetching the full message in the iOS example app. * Added per discussion in #93. * Fetching the full message is disabled by default.

--- example/ios/iOS UI Test/iOS UI Test/AppDelegate.m
+++ example/ios/iOS UI Test/iOS UI Test/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"FetchFullMessageEnabled"];
     return YES;
 }
 							
