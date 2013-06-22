e4cb9c41ebc8de388da788e4825eaef3ffc5f525
Paul Young <paulyoungonline@gmail.com>
2013-06-22 16:26:43 -0400

    Added switch to settings view controller. * Removed setting of user default in app delegate.

--- example/ios/iOS UI Test/iOS UI Test/AppDelegate.m
+++ example/ios/iOS UI Test/iOS UI Test/AppDelegate.m
@@ -13,7 +13,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"FetchFullMessageEnabled"];
     return YES;
 }
 							
--- example/ios/iOS UI Test/iOS UI Test/SettingsViewController.m
+++ example/ios/iOS UI Test/iOS UI Test/SettingsViewController.m
@@ -12,6 +12,7 @@
 NSString * const UsernameKey = @"username";
 NSString * const PasswordKey = @"password";
 NSString * const HostnameKey = @"hostname";
+NSString * const FetchFullMessageKey = @"FetchFullMessageEnabled";
 
 @implementation SettingsViewController
 
@@ -19,6 +20,7 @@ NSString * const HostnameKey = @"hostname";
 	[[NSUserDefaults standardUserDefaults] setObject:self.emailTextField.text ?: @"" forKey:UsernameKey];
 	[[FXKeychain defaultKeychain] setObject:self.passwordTextField.text ?: @"" forKey:PasswordKey];
 	[[NSUserDefaults standardUserDefaults] setObject:self.hostnameTextField.text ?: @"" forKey:HostnameKey];
+    [[NSUserDefaults standardUserDefaults] setBool:[self.fetchFullMessageSwitch isOn] forKey:FetchFullMessageKey];
 	
 	[self.delegate settingsViewControllerFinished:self];
 }
@@ -30,6 +32,7 @@ NSString * const HostnameKey = @"hostname";
 	self.emailTextField.text = [[NSUserDefaults standardUserDefaults] stringForKey:UsernameKey];
 	self.passwordTextField.text = [[FXKeychain defaultKeychain] objectForKey:PasswordKey];
 	self.hostnameTextField.text = [[NSUserDefaults standardUserDefaults] stringForKey:HostnameKey];
+    self.fetchFullMessageSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:FetchFullMessageKey];
 }
 
 @end
