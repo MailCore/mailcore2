MailCore/mailcore2

commit de51dd02d4c16d5c6bc89a2fe0e49ffda79f2a33

Fixed memory leak: operations were retained by the block they owned, which created a cycle reference.

src/objc/smtp/MCOSMTPSendOperation.mm
@@ -80,18 +80,6 @@ private:
     [self start];
 }
 
-- (void)operationCompleted {
-    if (_completionBlock == NULL)
-        return;
-    
-    nativeType *op = MCO_NATIVE_INSTANCE;
-    if (op->error() == mailcore::ErrorNone) {
-        _completionBlock(nil);
-    } else {
-        _completionBlock([NSError mco_errorWithErrorCode:op->error()]);
-    }
-}
-
 - (void) bodyProgress:(unsigned int)current maximum:(unsigned int)maximum
 {
     if (_progress != NULL) {

src/objc/imap/MCOIMAPAppendMessageOperation.mm
@@ -57,6 +57,8 @@ typedef void (^CompletionType)(NSError *error, uint32_t createdUID);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], 0);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 - (void) bodyProgress:(unsigned int)current maximum:(unsigned int)maximum

src/objc/imap/MCOIMAPCapabilityOperation.mm
@@ -53,6 +53,8 @@ typedef void (^CompletionType)(NSError *error, MCOIndexSet * capabilities);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], nil);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 @end

src/objc/imap/MCOIMAPCopyMessagesOperation.mm
@@ -54,6 +54,8 @@ typedef void (^CompletionType)(NSError *error, MCOIndexSet * destUids);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], 0);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 @end

src/objc/imap/MCOIMAPFetchContentOperation.mm
@@ -57,6 +57,8 @@ typedef void (^CompletionType)(NSError *error, NSData * data);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], nil);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 - (void) bodyProgress:(unsigned int)current maximum:(unsigned int)maximum

src/objc/imap/MCOIMAPFetchFoldersOperation.mm
@@ -58,6 +58,8 @@ typedef void (^CompletionType)(NSError *error, NSArray *folder);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], nil);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 @end

src/objc/imap/MCOIMAPFetchMessagesOperation.mm
@@ -53,6 +53,8 @@ typedef void (^CompletionType)(NSError *error, NSArray * messages, MCOIndexSet *
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], nil, nil);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 - (void) itemProgress:(unsigned int)current maximum:(unsigned int)maximum

src/objc/imap/MCOIMAPFetchNamespaceOperation.mm
@@ -53,6 +53,8 @@ typedef void (^CompletionType)(NSError *error, NSDictionary * namespaces);
     } else {
         _completionBlock([NSError mco_errorWithErrorCode:op->error()], nil);
     }
+    [_completionBlock release];
+    _completionBlock = nil;
 }
 
 @end

commit 9fe6ce12c4880a6c2f2a4180752987dd30d65c89

Fixed regression introduced in de51dd02d4c16d5c6bc89a2fe0e49ffda79f2a33

src/objc/smtp/MCOSMTPSendOperation.mm
@@ -80,6 +80,18 @@ private:
     [self start];
 }
 
+// This method needs to be duplicated from MCOSMTPOperation since _completionBlock
+// references the instance of this subclass and not the one from MCOSMTPOperation.
+- (void)operationCompleted {
+    if (_completionBlock == NULL)
+        return;
+    
+    NSError * error = [NSError mco_errorWithErrorCode:MCO_NATIVE_INSTANCE->error()];
+    _completionBlock(error);
+    [_completionBlock release];
+    _completionBlock = NULL;
+}
+
 - (void) bodyProgress:(unsigned int)current maximum:(unsigned int)maximum
 {
     if (_progress != NULL) {