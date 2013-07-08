1aa9d78484d0c2ee11f23b010c9ee19cc69765f1
CodaFi <devteam.codafi@gmail.com>
2013-07-08 09:18:56 -0600

    Patch copy

--- src/objc/abstract/MCOAbstractMessage.mm
+++ src/objc/abstract/MCOAbstractMessage.mm
@@ -46,12 +46,10 @@
 
 - (id) copyWithZone:(NSZone *)zone
 {
-    MCOAbstractMessage *message;
-    
-    message = [[[self class] alloc] initWithMCMessage:(mailcore::AbstractMessage *)_message->copy()->autorelease()];
-    [message setHeader:[[[self header] copy] autorelease]];
-    
-    return message;
+    nativeType * nativeObject = (nativeType *) [self mco_mcObject]->copy();
+    id result = [[self class] mco_objectWithMCObject:nativeObject];
+    MC_SAFE_RELEASE(nativeObject);
+    return [result retain];
 }
 
 - (void) dealloc
