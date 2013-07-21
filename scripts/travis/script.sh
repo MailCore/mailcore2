e02d50f88a583dacbbc957a03e5df9a5cf3441c6
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-07-21 13:37:35 -0700

    Debug info for Travis builds

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
+echo '** printenv **'
+printenv
+echo '** ls **'
+ls
+echo '** pwd **'
+pwd
 xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 ios" build
 xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 osx" build
