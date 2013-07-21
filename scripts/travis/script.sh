851a0d172f7c0b1546ac6fe672751da660cc6b69
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-07-21 13:49:23 -0700

    Travis: try to use xcodebuild

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-echo '** printenv **'
-printenv
-echo '** ls **'
-ls
-echo '** pwd **'
-pwd
-xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 ios" build
-xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 osx" build
+xcodebuild -list -project build-mac/mailcore2.xcodeproj
+
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "static mailcore2 ios" build
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "test-ios" build
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build
+
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build
+
+#xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 ios" build
+#xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 osx" build
