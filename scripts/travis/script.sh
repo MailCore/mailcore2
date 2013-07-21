d91eabc3d3ce259fd099236367cc2e08bf92ee80
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-07-21 14:13:46 -0700

    Disabled links test for iphoneos platform

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -5,11 +5,8 @@ xcodebuild -list -project build-mac/mailcore2.xcodeproj
 
 xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "static mailcore2 ios" build
 xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "test-ios" CODE_SIGN_IDENTITY="" build
+#xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "test-ios" CODE_SIGN_IDENTITY="" build
 xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build
 
 xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build
 xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build
-
-#xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 ios" build
-#xctool -project build-mac/mailcore2.xcodeproj -scheme "static mailcore2 osx" build
