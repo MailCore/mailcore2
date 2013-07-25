d90d68c7ba923fea3d805a9ebf872f84b6947e57
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-07-25 11:07:10 -0700

    Moved iOS MCOMessageView to src/ui/ios

---
+++ src/ui/ios/MCOMessageView.h
@@ -0,0 +1,50 @@
+//
+//  MCOMessageView.h
+//  testUI
+//
+//  Created by DINH Viêt Hoà on 1/19/13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#include <MailCore/MailCore.h>
+
+#import <UIKit/UIKit.h>
+
+@protocol MCOMessageViewDelegate;
+
+@interface MCOMessageView : UIView <UIWebViewDelegate>
+
+@property (nonatomic, copy) NSString * folder;
+@property (nonatomic, strong) MCOAbstractMessage * message;
+
+@property (nonatomic, assign) id <MCOMessageViewDelegate> delegate;
+
+@property (nonatomic, assign) BOOL prefetchIMAPImagesEnabled;
+@property (nonatomic, assign) BOOL prefetchIMAPAttachmentsEnabled;
+
+@end
+
+@protocol MCOMessageViewDelegate <NSObject>
+
+@optional
+- (NSData *) MCOMessageView:(MCOMessageView *)view dataForPartWithUniqueID:(NSString *)partUniqueID;
+- (void) MCOMessageView:(MCOMessageView *)view fetchDataForPartWithUniqueID:(NSString *)partUniqueID
+     downloadedFinished:(void (^)(NSError * error))downloadFinished;
+
+- (NSString *) MCOMessageView_templateForMainHeader:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForImage:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForAttachment:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForMessage:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForEmbeddedMessage:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForEmbeddedMessageHeader:(MCOMessageView *)view;
+- (NSString *) MCOMessageView_templateForAttachmentSeparator:(MCOMessageView *)view;
+
+- (NSDictionary *) MCOMessageView:(MCOMessageView *)view templateValuesForPartWithUniqueID:(NSString *)uniqueID;
+- (NSDictionary *) MCOMessageView:(MCOMessageView *)view templateValuesForHeader:(MCOMessageHeader *)header;
+- (BOOL) MCOMessageView:(MCOMessageView *)view canPreviewPart:(MCOAbstractPart *)part;
+
+- (NSString *) MCOMessageView:(MCOMessageView *)view filteredHTMLForPart:(NSString *)html;
+- (NSString *) MCOMessageView:(MCOMessageView *)view filteredHTMLForMessage:(NSString *)html;
+- (NSData *) MCOMessageView:(MCOMessageView *)view previewForData:(NSData *)data isHTMLInlineImage:(BOOL)isHTMLInlineImage;
+
+@end
