a7f4d851239574e1f7ce2eeaa8a4ddc3764bb1b8
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-07-27 22:30:37 -0700

    Shorter logs

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-xcodebuild -list -project build-mac/mailcore2.xcodeproj
-
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "static mailcore2 ios" build
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build
+echo Building library for iPhoneOS
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "static mailcore2 ios" build > /dev/null
+echo Building library for iPhoneSimulator
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build > /dev/null
+#echo Link test for iPhoneOS
 #xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "test-ios" CODE_SIGN_IDENTITY="" build
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build
+echo Link test for iPhoneSimulator
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build > /dev/null
 
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build
+echo Building library for Mac
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build > /dev/null
+echo Building framework for Mac
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "mailcore2 osx" build > /dev/null
+echo Link test for Mac
+xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build > /dev/null
