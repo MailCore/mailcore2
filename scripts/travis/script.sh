77943c1c952b2bb6304017e80ea920cad8ed2e26
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2013-08-25 00:46:08 -0700

    Made scheme visible for xctool

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -5,23 +5,20 @@ echo Building library for iPhoneOS
 pwd
 xctool -version
 #xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "static mailcore2 ios" build > /dev/null
-cd build-mac
-xctool -project mailcore2.xcodeproj -sdk iphoneos6.1 -scheme "static mailcore2 ios" build
-cd ..
 xctool -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -scheme "static mailcore2 ios" build
 echo Building library for iPhoneSimulator
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build > /dev/null
+xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "static mailcore2 ios" build
 #echo Link test for iPhoneOS
 #xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos6.1 -target "test-ios" CODE_SIGN_IDENTITY="" build
 echo Link test for iPhoneSimulator
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build > /dev/null
+xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator6.1 -target "test-ios" build
 
 echo Building library for Mac
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build > /dev/null
+xctool -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "static mailcore2 osx" build
 echo Building framework for Mac
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "mailcore osx" build > /dev/null
+xctool -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "mailcore osx" build
 echo Link test for Mac
-xcodebuild -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build > /dev/null
+xctool -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -target "tests" build
 
 echo Testing CMake build
 mkdir -p build-cmake
