d4bca3a8fbb6f77c32b0ef784b77ebb172cdd6fd
Matt Ronge <mronge@mronge.com>
2013-09-26 09:37:14 -0500

    Only build arm64 if 7.0 SDK is present

--- scripts/prepare-ctemplate-ios.sh
+++ scripts/prepare-ctemplate-ios.sh
@@ -4,8 +4,10 @@ url="https://github.com/dinhviethoa/ctemplate"
 
 if xcodebuild -showsdks|grep iphoneos6.1 >/dev/null ; then
 	sdkversion=6.1
+         MARCHS="armv7 armv7s"
 elif xcodebuild -showsdks|grep iphoneos7.0 >/dev/null ; then
 	sdkversion=7.0
+         MARCHS="armv7 armv7s arm64"
 else
 	echo SDK not found
 	exit 1
@@ -70,7 +72,6 @@ sdk="iphoneos$sdkversion"
 sysroot="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$sdkversion.sdk"
 
 ARCH=arm
-MARCHS="armv7 armv7s"
 for MARCH in $MARCHS; do
   echo "$logdir/ctemplate-build.log"
   export CFLAGS="-arch ${MARCH} -isysroot $sysroot"
