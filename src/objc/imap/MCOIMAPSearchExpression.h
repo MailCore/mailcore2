e38e72a6cf228bd940d73f269e3b07e397a67292
CodaFi <devteam.codafi@gmail.com>
2013-11-24 13:05:16 -0700

    ObjC Interface

--- src/objc/imap/MCOIMAPSearchExpression.h
+++ src/objc/imap/MCOIMAPSearchExpression.h
@@ -72,6 +72,156 @@
 + (MCOIMAPSearchExpression *) searchHeader:(NSString *)header value:(NSString *)value;
 
 /**
+ Creates a search expression that matches messages with the Read flag.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchRead;
+
+/**
+ Creates a search expression that matches messages without the Read flag.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchUnread;
+
+/**
+ Creates a search expression that matches messages that have been flagged.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchFlagged;
+
+/**
+ Creates a search expression that matches messages that haven't been flagged.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchUnflagged;
+
+/**
+ Creates a search expression that matches messages that have the answered flag set.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchAnswered;
+
+/**
+ Creates a search expression that matches messages that don't have the answered flag set..
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchUnanswered;
+
+/**
+ Creates a search expression that matches draft messages.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchDraft;
+
+/**
+ Creates a search expression that matches messages that aren't drafts.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchUndraft;
+
+/**
+ Creates a search expression that matches messages that are deleted.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchDeleted;
+
+/**
+ Creates a search expression that matches messages that are spam.
+ 
+ Example:
+ 
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchHeader:@"List-Id" value:@"shoes"]
+ **/
++ (MCOIMAPSearchExpression *) searchSpam;
+
+/**
+ Creates a search expression that matches messages sent before a date.
+ 
+ Example:
+ 
+ NSDate *yesterday = [NSDate dateWithTimeIntervalSinceNow:-(60.0 * 60.0 * 24.0)];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchBeforeDate:yesterday]
+ **/
++ (MCOIMAPSearchExpression *) searchBeforeDate:(NSDate *)date;
+
+/**
+ Creates a search expression that matches messages sent on a date.
+ 
+ Example:
+ 
+ NSDate *now = [NSDate date];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchOnDate:now]
+ **/
++ (MCOIMAPSearchExpression *) searchOnDate:(NSDate *)date;
+
+/**
+ Creates a search expression that matches messages sent since a date.
+ 
+ Example:
+ 
+ NSDate *now = [NSDate date];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchSinceDate:now]
+ **/
++ (MCOIMAPSearchExpression *) searchSinceDate:(NSDate *)date;
+
+/**
+ Creates a search expression that matches messages recieved before a date.
+ 
+ Example:
+ 
+ NSDate *yesterday = [NSDate dateWithTimeIntervalSinceNow:-(60.0 * 60.0 * 24.0)];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchBeforeRecievedDate:yesterday]
+ **/
++ (MCOIMAPSearchExpression *) searchBeforeRecievedDate:(NSDate *)date;
+
+/**
+ Creates a search expression that matches messages recieved on a date.
+ 
+ Example:
+ 
+ NSDate *now = [NSDate date];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchOnRecievedDate:now]
+ **/
++ (MCOIMAPSearchExpression *) searchOnRecievedDate:(NSDate *)date;
+
+/**
+ Creates a search expression that matches messages recieved since a date.
+ 
+ Example:
+ 
+ NSDate *yesterday = [NSDate dateWithTimeIntervalSinceNow:-(60.0 * 60.0 * 24.0)];
+ MCOIMAPSearchExpression * expr = [MCOIMAPSearchExpression searchSinceReciviedDate:yesterday]
+ **/
++ (MCOIMAPSearchExpression *) searchSinceReciviedDate:(NSDate *)date;
+
+/**
  Creates a search expression that matches emails with the given gmail thread id
  
  Example:
