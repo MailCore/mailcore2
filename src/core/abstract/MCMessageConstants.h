11b0f25e3a55da0f972de5fdc634d488a34d577e
pfennema <paul@bryce.demon.nl>
2013-12-01 21:33:55 +0100

    Changed MCOIMAPMessagesRequestKindSize to camelCase.

--- src/core/abstract/MCMessageConstants.h
+++ src/core/abstract/MCMessageConstants.h
@@ -69,7 +69,7 @@ namespace mailcore {
         IMAPMessagesRequestKindGmailMessageID = 1 << 7,
         IMAPMessagesRequestKindGmailThreadID  = 1 << 8,
         IMAPMessagesRequestKindExtraHeaders  = 1 << 9,
-        IMAPMessagesRequestKINDSize          = 1 << 10,
+        IMAPMessagesRequestKindSize          = 1 << 10,
     };
 
     enum IMAPFetchRequestType {
