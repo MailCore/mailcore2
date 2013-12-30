f6cf583d30c472211fff5519a884d19d6ff7888e
Robert Widmann <devteam.codafi@gmail.com>
2013-12-29 18:10:00 -0800

    Remove mentions of register
    C++ 11 Standard (Annex D.2):
    
    The use of the register keyword as a storage-class-specifier (7.1.1) is
    deprecated.

--- src/core/basetypes/MCHash.cc
+++ src/core/basetypes/MCHash.cc
@@ -1,8 +1,8 @@
 #include "MCHash.h"
 
 unsigned int mailcore::hashCompute(const char * key, unsigned int len) {
-  register unsigned int c = 5381;
-  register const char * k = key;
+  unsigned int c = 5381;
+  const char * k = key;
   
   while (len--) {
     c = ((c << 5) + c) + *k++;
