29e5daef6d0ba242c992b989eb7b608bd1b6a323
lgarbo <garbo@email.it>
2014-01-30 18:46:07 +0100

    Update MCOIMAPNamespace.mm

--- src/objc/imap/MCOIMAPNamespace.mm
+++ src/objc/imap/MCOIMAPNamespace.mm
@@ -89,7 +89,7 @@
 
 - (NSArray *) prefixes
 {
-    return MCO_TO_OBJC(MCO_NATIVE_INSTANCE->prefixes());
+    return MCO_OBJC_BRIDGE_GET(prefixes);
 }
 
 - (NSString *) pathForComponents:(NSArray *)components
