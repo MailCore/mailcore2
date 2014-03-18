8871a6cb9ac3e2713e03c78ee0275a35f1835efb
Matt Ronge <mronge@mronge.com>
2014-03-18 13:44:12 -0500

    Fix crasher where mMainPart was NULL

--- src/core/imap/MCIMAPMessage.cc
+++ src/core/imap/MCIMAPMessage.cc
@@ -243,7 +243,9 @@ HashMap * IMAPMessage::serializable()
     if (customFlags() != NULL) {
         result->setObjectForKey(MCSTR("customFlags"), customFlags());
     }
-    result->setObjectForKey(MCSTR("mainPart"), mMainPart->serializable());
+    if (mMainPart != NULL) {
+        result->setObjectForKey(MCSTR("mainPart"), mMainPart->serializable());
+    }
     if (gmailLabels() != NULL) {
         result->setObjectForKey(MCSTR("gmailLabels"), gmailLabels());
     }
