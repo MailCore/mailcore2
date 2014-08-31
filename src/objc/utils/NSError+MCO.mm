634bbb6bc3a8f24bb32d5f7a0b5691a0caf606b8
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-08-30 18:39:47 -0700

    Fixed warning (fixed #852)

--- src/objc/utils/NSError+MCO.mm
+++ src/objc/utils/NSError+MCO.mm
@@ -57,7 +57,7 @@ static NSString * MCOLocalizedDescriptionTable[] = {
     }
     
     NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
-    if (code < MCOErrorCodeCount) {
+    if ((NSInteger) code < MCOErrorCodeCount) {
         NSString * localizedString = NSLocalizedStringFromTable(MCOLocalizedDescriptionTable[code], @"description of errors of mailcore", @"MailCore");
         [userInfo setObject:localizedString forKey:NSLocalizedDescriptionKey];
     }
