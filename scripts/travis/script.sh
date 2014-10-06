64bc9026df808589df5e6ac409461019b6705d17
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-10-05 21:48:35 -0700

    iOS 7.1 -> iOS 8.0

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -6,13 +6,13 @@ uname
 
 if test x"`uname`" = xDarwin ; then
   echo Building library for iPhoneOS
-  xctool -project build-mac/mailcore2.xcodeproj -sdk iphoneos7.1 -scheme "static mailcore2 ios" build ARCHS="armv7 armv7s arm64"
+  xctool -project build-mac/mailcore2.xcodeproj -sdk iphoneos8.0 -scheme "static mailcore2 ios" build ARCHS="armv7 armv7s arm64"
   echo Building library for iPhoneSimulator
-  xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator7.1 -scheme "static mailcore2 ios" build ARCHS="i386 x86_64"
+  xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator8.0 -scheme "static mailcore2 ios" build ARCHS="i386 x86_64"
   #echo Link test for iPhoneOS
   #xcodebuild -project build-mac/mailcore2.xcodeproj -sdk iphoneos7.1 -target "test-ios" CODE_SIGN_IDENTITY="" build
   echo Link test for iPhoneSimulator
-  xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator7.1 -scheme "test-ios" build ARCHS="i386 x86_64"
+  xctool -project build-mac/mailcore2.xcodeproj -sdk iphonesimulator8.0 -scheme "test-ios" build ARCHS="i386 x86_64"
 
   echo Building library for Mac
   xctool -project build-mac/mailcore2.xcodeproj -sdk macosx10.8 -scheme "static mailcore2 osx" build
