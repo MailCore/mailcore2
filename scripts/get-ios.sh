da097788fc9d80ed0473dd634b26b3ae281e5618
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-10-27 23:00:18 -0700

    Made it easier to build mailcore

--- scripts/get-ios.sh
+++ scripts/get-ios.sh
@@ -11,3 +11,19 @@ for dep in $deps ; do
   name="$dep"
   get_prebuilt_dep
 done
+
+deps+=" libsasl-ios"
+if test "x$CONFIGURATION_BUILD_DIR" != x ; then
+  mkdir -p "$CONFIGURATION_BUILD_DIR"
+  cd "$scriptpath/../Externals"
+  for dep in $deps ; do
+    if test -d "$dep" ; then
+      if test -d "$dep"/lib ; then
+        rsync -a "$dep"/lib/ "$CONFIGURATION_BUILD_DIR"
+      fi
+      if test -d "$dep"/include ; then
+        rsync -a "$dep"/include/ "$CONFIGURATION_BUILD_DIR/include/"
+      fi
+    fi
+  done
+fi
