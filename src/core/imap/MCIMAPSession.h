bcc36e5a33593d30df8904ae880d7999b7723499
Lin Zhan <zhanleewo@gmail.com>
2015-03-25 14:26:16 +0800

    The workaround for 163.com
    The imap service provided by 163.com could not be access.
    required: the libetpan need update to 050f15f30ce40c02d4cf0a9389baca77dec19230

--- src/core/imap/MCIMAPSession.h
+++ src/core/imap/MCIMAPSession.h
@@ -59,6 +59,9 @@ namespace mailcore {
         virtual void setVoIPEnabled(bool enabled);
         virtual bool isVoIPEnabled();
         
+        virtual void setNetEaseWorkaroundEnabled(bool enabled);
+        virtual bool isNetEaseWorkaroundEnabled();
+        
         // Needed for fetchSubscribedFolders() and fetchAllFolders().
         virtual void setDefaultNamespace(IMAPNamespace * ns);
         virtual IMAPNamespace * defaultNamespace();
