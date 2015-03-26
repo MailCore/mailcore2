8f6e26cf8a3f3baf7d309e89dde620e9f303157f
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2015-03-25 17:30:54 -0700

    Updated to libetpan 13de63f24954c0541ba93675f905ffcccd455d6c

--- scripts/build-libetpan-ios.sh
+++ scripts/build-libetpan-ios.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 . "$scriptpath/include.sh/build-dep.sh"
 
 url="https://github.com/dinhviethoa/libetpan.git"
-rev=c3d37fa8a135c36d9f296f01585e56b94dd55642
+rev=13de63f24954c0541ba93675f905ffcccd455d6c
 name="libetpan-ios"
 xcode_target="libetpan ios"
 xcode_project="libetpan.xcodeproj"
