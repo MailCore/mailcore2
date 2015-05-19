96772b7161ce9a191782c260f54ab3b810ca9d8c
Antoine Cœur <coeur@gmx.fr>
2015-05-19 08:13:16 +0800

    spelling fix

--- src/core/abstract/MCErrorMessage.cpp
+++ src/core/abstract/MCErrorMessage.cpp
@@ -15,34 +15,34 @@ static const char * localizedDescriptionTable[] = {
     "MobileMe is no longer an active mail service.",                                 /** MCOErrorMobileMeMoved */
     "Yahoo!'s servers are currently unavailable.",                                   /** MCOErrorYahooUnavailable */
     "The requested folder does not exist.  Folder selection failed",                 /** MCOErrorNonExistantFolder */
-    "An error occured while renaming the requested folder.",                         /** MCOErrorRename */
-    "An error occured while deleting the requested folder.",                         /** MCOErrorDelete */
-    "An error occured while creating the requested folder.",                         /** MCOErrorCreate */
-    "An error occured while (un)subscribing to the requested folder.",               /** MCOErrorSubscribe */
-    "An error occured while appending a message to the requested folder.",           /** MCOErrorAppend */
-    "An error occured while copying a message to the requested folder.",             /** MCOErrorCopy */
-    "An error occured while expunging a message in the requested folder.",           /** MCOErrorExpunge */
-    "An error occured while fetching messages in the requested folder.",             /** MCOErrorFetch */
-    "An error occured during an IDLE operation.",                                    /** MCOErrorIdle */
-    "An error occured while requesting the server's identity.",                      /** MCOErrorIdentity */
-    "An error occured while requesting the server's namespace.",                     /** MCOErrorNamespace */
-    "An error occured while storing flags.",                                         /** MCOErrorStore */
-    "An error occured while requesting the server's capabilities.",                  /** MCOErrorCapability */
+    "An error occurred while renaming the requested folder.",                         /** MCOErrorRename */
+    "An error occurred while deleting the requested folder.",                         /** MCOErrorDelete */
+    "An error occurred while creating the requested folder.",                         /** MCOErrorCreate */
+    "An error occurred while (un)subscribing to the requested folder.",               /** MCOErrorSubscribe */
+    "An error occurred while appending a message to the requested folder.",           /** MCOErrorAppend */
+    "An error occurred while copying a message to the requested folder.",             /** MCOErrorCopy */
+    "An error occurred while expunging a message in the requested folder.",           /** MCOErrorExpunge */
+    "An error occurred while fetching messages in the requested folder.",             /** MCOErrorFetch */
+    "An error occurred during an IDLE operation.",                                    /** MCOErrorIdle */
+    "An error occurred while requesting the server's identity.",                      /** MCOErrorIdentity */
+    "An error occurred while requesting the server's namespace.",                     /** MCOErrorNamespace */
+    "An error occurred while storing flags.",                                         /** MCOErrorStore */
+    "An error occurred while requesting the server's capabilities.",                  /** MCOErrorCapability */
     "The server does not support STARTTLS connections.",                             /** MCOErrorStartTLSNotAvailable */
     "Attempted to send a message with an illegal attachment.",                       /** MCOErrorSendMessageIllegalAttachment */
     "The SMTP storage limit was hit while trying to send a large message.",          /** MCOErrorStorageLimit */
     "Sending messages is not allowed on this server.",                               /** MCOErrorSendMessageNotAllowed */
     "The current HotMail account cannot connect to WebMail.",                        /** MCOErrorNeedsConnectToWebmail */
-    "An error occured while sending the message.",                                   /** MCOErrorSendMessage */
+    "An error occurred while sending the message.",                                   /** MCOErrorSendMessage */
     "Authentication is required for this SMTP server.",                              /** MCOErrorAuthenticationRequired */
-    "An error occured while fetching a message list on the POP server.",             /** MCOErrorFetchMessageList */
-    "An error occured while deleting a message on the POP server.",                  /** MCOErrorDeleteMessage */
+    "An error occurred while fetching a message list on the POP server.",             /** MCOErrorFetchMessageList */
+    "An error occurred while deleting a message on the POP server.",                  /** MCOErrorDeleteMessage */
     "Account check failed because the account is invalid.",                          /** MCOErrorInvalidAccount */
     "File access error",                                                             /** MCOErrorFile */
     "Compression is not available",                                                  /** MCOErrorCompression */
     "A sender is required to send message",                                          /** MCOErrorNoSender */
     "A recipient is required to send message",                                       /** MCOErrorNoRecipient */
-    "An error occured while performing a No-Op operation.",                          /** MCOErrorNoop */
+    "An error occurred while performing a No-Op operation.",                          /** MCOErrorNoop */
     "An application specific password is required",                                  /** MCOErrorGmailApplicationSpecificPasswordRequired */
     "An error when requesting date",                                                 /** MCOErrorServerDate */
     "No valid server found",                                                         /** MCOErrorNoValidServerFound */
