b93062e0e918f7924fa48724fdbf53d91aa348bb
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2015-07-13 15:38:38 -0700

    Added missing files

---
+++ src/core/zip/MCZipPrivate.h
@@ -0,0 +1,20 @@
+//
+//  MCZipMac.h
+//  mailcore2
+//
+//  Created by juliangsp on 7/7/15.
+//  Copyright (c) 2015 MailCore. All rights reserved.
+//
+
+#ifndef mailcore2_MCZipMac_h
+#define mailcore2_MCZipMac_h
+
+#include <MailCore/MCBaseTypes.h>
+
+namespace mailcore {
+    
+    String * TemporaryDirectoryForZip();
+    
+};
+
+#endif
