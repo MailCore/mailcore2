5d457fe0c3aa056d3964eddd274df5aafd9b5f1b
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2016-01-01 22:35:01 -0800

    Travis: fixed build on Linux (uuid and ctemplate)

--- scripts/travis/before-script.sh
+++ scripts/travis/before-script.sh
@@ -13,13 +13,24 @@ elif test x"`uname`" = xLinux ; then
   make >/dev/null
   sudo make install >/dev/null
   cd ..
+  git clone --depth=1 https://github.com/dinhviethoa/ctemplate
+  cd ctemplate
+  ./autogen.sh
+  ./configure
+  make >/dev/null
+  sudo make install >/dev/null
+  cd ..
+  curl -O http://ftp.kernel.org/pub/linux/utils/util-linux/v2.27/util-linux-2.27.1.tar.xz
+  tar xf util-linux-2.27.1.tar.xz
+  cd util-linux-2.27.1
+  ./configure
+  make libuuid.la
+  sudo make install-uuidincHEADERS
+  sudo cp .libs/libuuid.so /usr/lib
+  cd ..
 
-  sudo apt-get install libctemplate-dev
   sudo apt-get install libicu-dev
-  sudo apt-get install libsasl2-dev
   sudo apt-get install libtidy-dev
-  sudo apt-get install uuid-dev
   sudo apt-get install libxml2-dev
-  sudo apt-get install libuchardet-dev
 fi
 
