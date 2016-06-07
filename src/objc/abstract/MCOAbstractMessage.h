dd7a217cd03b110c68ab4ef89a4e4c94776d3895
Ilya Laryionau <larryonoff@gmail.com>
2016-06-07 13:23:07 +0300

    attempt to fix Travis build

--- src/objc/abstract/MCOAbstractMessage.h
+++ src/objc/abstract/MCOAbstractMessage.h
@@ -15,14 +15,6 @@
 @class MCOMessageHeader;
 @class MCOAbstractPart;
 
-#if __has_feature(objc_generics)
-#  define MCO_GENERIC(...)  <__VA_ARGS__>
-#  define MCO_GENERIC_KINDOF(...) <__kindof __VA_ARGS__>
-#else
-#  define MCO_GENERIC(...)
-#  define MCO_GENERIC_KINDOF(...)
-#endif
-
 #ifdef __cplusplus
 namespace mailcore {
     class AbstractMessage;
@@ -48,19 +40,19 @@ namespace mailcore {
  It will return an array of MCOIMAPPart for MCOIMAPMessage.
  It will return an array of MCOAttachment for MCOMessageParser.
  It will return an array of MCOAttachment for MCOMessageBuilder. */
-- (NSArray MCO_GENERIC_KINDOF(MCOAbstractPart *) *) attachments;
+- (NSArray<__kindof MCOAbstractPart *> *) attachments;
 
 /** All image attachments included inline in the message through cid: URLs.
  It will return an array of MCOIMAPPart for MCOIMAPMessage.
  It will return an array of MCOAttachment for MCOMessageParser.
  It will return an array of MCOAttachment for MCOMessageBuilder. */
-- (NSArray MCO_GENERIC_KINDOF(MCOAbstractPart *) *) htmlInlineAttachments;
+- (NSArray<__kindof MCOAbstractPart *> *) htmlInlineAttachments;
 
 /**
  Returns parts required to render the message as plain text or html.
  This does not include inline images and attachments, but only the text content
  */
-- (NSArray MCO_GENERIC_KINDOF(MCOAbstractPart *) *) requiredPartsForRendering;
+- (NSArray<__kindof MCOAbstractPart *> *) requiredPartsForRendering;
 
 @end
 
