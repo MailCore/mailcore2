cfbfbbcc0a41585a79a1a1fb4d975c2a11f206b1
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2016-11-19 16:53:03 -0800

    Updated libetpan to 07508b7e44949dfc09d3494c5409b54ef96604d8

--- scripts/build-libetpan-osx.sh
+++ scripts/build-libetpan-osx.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 . "$scriptpath/include.sh/build-dep.sh"
 
 url="https://github.com/dinhviethoa/libetpan.git"
-rev=4c36ec1850b6b7bdab9c22008500ca8f47fdf550
+rev=07508b7e44949dfc09d3494c5409b54ef96604d8
 name="libetpan-osx"
 xcode_target="static libetpan"
 xcode_project="libetpan.xcodeproj"
