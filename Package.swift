901633c675fe51e04a52be3386c77ad2dcc0d875
Matthew Maddux <mattmaddux@Matthews-MacBook-Air.local>
2022-08-02 22:16:31 -0500

    Updated Package.swift with new GitHub-hosted binary path.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "MailCore2",
-                      url: "https://downloads.maddux.cloud/mailcore2-apple-xcframework/MailCore2-2020-09-24.xcframework.zip",
+                      url: "https://github.com/MailCore/mailcore2/raw/master/bin/MailCore2-2020-09-24.xcframework.zip",
                       checksum: "c3479968c758094165fb0b4de5ca7dd9f8aafac423388c51406c447f69a1b853")
     ]
 )
